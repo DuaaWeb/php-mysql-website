e7a511387e2fd143b316798ec70d5b5e6acc9302
Ragip Mullamusa <69114250+fonixweb@users.noreply.github.com>
2021-04-14 21:50:04 +0300

    Update dbcon.php

--- www/dbcon.php
+++ www/dbcon.php
@@ -1,7 +1,6 @@
 <?php
 $con = mysqli_connect("94.73.148.70","u9916004_ragip","PGra85V6TLpu64D","u9916004_fonix_w");
 $con->set_charset("utf8");
-// Check connection
 if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
